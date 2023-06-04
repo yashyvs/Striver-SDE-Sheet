92a436aae71916d4afe907e6a6c235194ca74eae
Yash Vardhan Singh <101694025+yashyvs@users.noreply.github.com>
2023-06-04 13:44:07 +0530

    Create 01_Set_Matrix_Zeros.cpp

---
+++ Day-01/01_Set_Matrix_Zeros.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+void setZeros(vector<vector<int>> &matrix)
+{
+    int row = matrix.size(), col = matrix[0].size(), col0 = 1;
+    for(int i = 0; i < row; i++){
+        if(!matrix[i][0]) col0 = 0;
+        for (int j = 1; j < col; j++) {
+          if (!matrix[i][j])
+            matrix[0][j] = matrix[i][0] = 0;
+        }
+    }
+        
+    for(int i = row-1; i >= 0; i--){
+      for (int j = col - 1; j >= 1; j--) {
+        if (!matrix[i][0] || !matrix[0][j])
+          matrix[i][j] = 0;
+      	}
+        if(!col0) matrix[i][0] = 0;
+    }
+}
