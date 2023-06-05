0edc73437d1fbbd3e60bdf73d2a2aff9801e94ef
Yash Vardhan Singh <101694025+yashyvs@users.noreply.github.com>
2023-06-05 13:40:21 +0530

    Create 09_Merge_Two_Sorted_Arrays.cpp

---
+++ Day-02/09_Merge_Two_Sorted_Arrays.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, int n) {
+	int i = 0;
+	int j = 0;
+
+	vector<int> ans;
+
+	while(i<m && j<n) {
+
+		if(arr1[i] < arr2[j]) ans.push_back(arr1[i++]);
+		else ans.push_back(arr2[j++]);
+	}
+
+	while(i<m) {
+		ans.push_back(arr1[i++]);
+	}
+
+    while(j<n) {
+      ans.push_back(arr2[j++]);
+    }
+
+    return ans;
+}
