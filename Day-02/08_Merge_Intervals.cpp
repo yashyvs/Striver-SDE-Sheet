f1751986a84a43c7e41077a0939fc4cd274beb36
Yash Vardhan Singh <101694025+yashyvs@users.noreply.github.com>
2023-06-05 13:36:15 +0530

    Create 08_Merge_Intervals.cpp

---
+++ Day-02/08_Merge_Intervals.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+/*
+
+    intervals[i][0] = start point of i'th interval
+    intervals[i][1] = finish point of i'th interval
+
+*/
+
+vector<vector<int>> mergeIntervals(vector<vector<int>> &intervals)
+{
+    sort(intervals.begin(), intervals.end());
+    int n = intervals.size();
+
+    vector<vector<int>> ans;
+    for (int i=0; i<n; i++) {
+        if (ans.empty() || ans.back()[1] < intervals[i][0]) {
+            ans.push_back(intervals[i]);
+        }
+        else {
+            ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+        }
+    }
+    return ans;
+}
